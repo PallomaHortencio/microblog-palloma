65cc436a28e98dcd12117c0c169e8d2ead1ed1b3
Palloma Hortencio <palloma.hortencio@gmail.com>
2022-07-27 08:41:33 -0300

    Excluir
    função de excluir usuario

---
+++ admin/usuario-exclui.php
@@ -0,0 +1,20 @@
+<?php
+
+use Microblog\Usuario;
+
+require_once "../vendor/autoload.php";
+
+
+// criamos um objeto para poder acessar os recursos da classe
+$usuario = new Usuario; // não esqueça do autoload e de namespace
+
+// obtemos o id da url e o passamos para o setter
+$usuario->setId($_GET['id']);
+
+// só então executamos o método de exclusão
+$usuario->excluir();
+
+// após excluir, redirecionamos para a página de lista de usuários
+header("location:usuarios.php");
+
+?>
--- src/Usuario.php
+++ src/Usuario.php
@@ -105,6 +105,19 @@ final class Usuario {
     }
 
 
+    public function excluir():void{
+        $sql = "DELETE FROM usuarios WHERE id = :id";
+        
+        try {
+            $consulta = $this->conexao->prepare($sql);
+            $consulta->bindParam(":id", $this->id, PDO::PARAM_INT);
+            $consulta->execute();
+
+        } catch (Exception $erro) {
+            die("Erro: ". $erro->getMessage());
+        }
+    }
+
 
     public function getId(): int
     {
